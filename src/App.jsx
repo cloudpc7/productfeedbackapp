23426452be52c05c809eb3262f894f9285311809
cloudpc7 <clouddropdesigns@gmail.com>
2026-02-28 12:57:04 -0800

    removing git folder in src folder

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Hello World</h1>
+    <h2>Testing deployment</h2>
+    </>
+  )
+}
+
+export default App
